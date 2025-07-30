78f68971be20ebfa55ecf80dcdb3718eb8d9208f
Abdullah Azeem <163125247+Abdullah-Azeem18@users.noreply.github.com>
2025-07-30 22:55:01 +0500

    Add files via upload

---
+++ Assignment#2/app.js
@@ -0,0 +1,62 @@
+// 
+
+
+
+
+
+
+// // Question 3
+// const products = [
+//   { name: "Laptop", price: 800 },
+//   { name: "Mouse", price: 20 },
+//   { name: "Phone", price: 500 },
+// ];
+
+// function filter(min,max){
+
+// }
+
+
+
+
+
+
+
+
+
+// // Question  4
+// const str="madam";
+// const palindrome=str.split("").reverse().join("");
+// if(palindrome==str){
+//     console.log(`${palindrome} is a palindrome `);
+// }
+// else{
+//     console.log(`${palindrome} is not a palindrome `);
+// }
+
+ // Question 5
+// const nestedArray = [1, [2, [3, 4]], 5];
+// const newarray1=nestedArray.flat();
+// console.log(newarray1);
+// const newarray2=nestedArray.flat(2);
+// console.log(newarray2);
+
+// Question 6
+// const employees = [
+//   { name: "Ali", salary: 1000 },
+//   { name: "Zara", salary: 1500 },
+//   { name: "Ahmed", salary: 1200 },
+// ];
+//   const total_salary=employees.reduce((currentsalary,nextsalary)=>currentsalary+nextsalary.salary,0);
+// console.log(total_salary);
+
+// //   Question 10
+// const data = [1, 2, [4, 5, [6, 8]], 10];
+// const newdata1=data.flat();
+// const newdata2=data.flat(2);
+// const sum=newdata2.reduce((sum,num)=>{
+//     return num%2==0 ? sum+num: sum},0);
+//     console.log(sum);
+    
+
+
